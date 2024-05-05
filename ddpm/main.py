eb234e4762b529c2cc617dfe46ec9af8986ad0ce
Jing Wu <wwujing123@gmail.com>
2024-05-05 20:23:05 +1000

    Update main.py

--- ddpm/main.py
+++ ddpm/main.py
@@ -7,7 +7,7 @@ import torch
 import numpy as np
 
 from runners.diffusion import Diffusion
-from functions import get_config_and_setup_dirs
+from functions import get_config_and_setup_dirs, get_mask_config_and_setup_dirs
 
 torch.set_printoptions(sci_mode=False)
 
@@ -18,18 +18,34 @@ def parse_args_and_config():
     parser.add_argument('--unl_ratio', default=0.1, type=float)
     parser.add_argument('--unl_cls', default='0', type=str)
     parser.add_argument('--num_workers', type=int, default=4)
-    parser.add_argument('--train_batch_size', type=int, default=512)
-    parser.add_argument('--unl_eps', type=int, default=10)
+    parser.add_argument('--train_batch_size', type=int, default=128)
+    parser.add_argument('--unl_eps', type=int, default=4)
     parser.add_argument('--K_steps', default=2, type=int, help='K steps for LL')
     parser.add_argument('--S_steps', default=2, type=int, help='S steps for UL')
     parser.add_argument('--lambda_bome', type=float, default=0.1)
+    parser.add_argument('--eta_bome', type=float, default=0.1)
+    parser.add_argument('--rem_num', type=int, default=4500)
+    # SalUn
+    parser.add_argument(
+        "--alpha", type=float, default=1.0, help="forget loss vs remain loss"
+    )
+    parser.add_argument(
+        "--mask_path", type=str, default=None, help="the path to store mask"
+    )
+    parser.add_argument(
+        "--mask_ratio", type=float, default=0.5, help="mask ratio for unlearning"
+    )
+    parser.add_argument(
+        "--method", type=str, default=None, help="the method to unlearn"
+    )
     #
     parser.add_argument(
         "--config", type=str, required=True, help="Path to the config file"
     )
     parser.add_argument("--ckpt_folder", type=str, default='./results/cifar10')
     parser.add_argument(
-        "--mode", type=str, default='train', help="['train', 'forget', 'erasediff']"
+        "--mode", type=str, default='train',
+        help="['train', 'forget', 'ours', 'ft', 'ng', 'blindspot', 'saliency_unlearn']"
     )
     parser.add_argument(
         "--label_to_forget", type=int, default=0, help="Class label 0-9 to forget. Only for forgetting training."
@@ -72,7 +88,14 @@ def parse_args_and_config():
     parser.add_argument("--sequence", action="store_true")
 
     args = parser.parse_args()
-    config = get_config_and_setup_dirs(os.path.join("configs", args.config))
+    # config = get_config_and_setup_dirs(os.path.join("configs", args.config))
+    if args.mode == "saliency_unlearn":
+        config = get_mask_config_and_setup_dirs(
+            args, os.path.join("configs", args.config)
+        )
+    else:
+        config = get_config_and_setup_dirs(os.path.join("configs", args.config))
+
 
     # setup logger
     level = getattr(logging, args.verbose.upper(), None)
@@ -109,10 +132,20 @@ def main():
 
     try:
         runner = Diffusion(args, config)
-        if args.mode == 'erasediff':
-            runner.train_erasediff()
+        if args.mode == 'ours':
+            runner.train_ours()
         elif args.mode == 'forget':
             runner.train_forget()
+        elif args.mode == 'ft':
+            runner.train_ft()
+        elif args.mode == 'ng':
+            runner.train_ng()
+        elif args.mode == 'blindspot':
+            runner.train_blindspot()
+        elif args.mode == "saliency_unlearn":
+            runner.saliency_unlearn()
+        elif args.mode == "generate_mask":
+            runner.generate_mask()
     except Exception:
         logging.error(traceback.format_exc())
 
